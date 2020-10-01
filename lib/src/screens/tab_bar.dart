fae4c0fcfabc92cb1b77a7580df0cab24ee69b03
BRO3886 <siddverma1999@gmail.com>
2020-10-01 14:06:14 +0530

    made bottom navbar functional

--- lib/src/screens/tab_bar.dart
+++ lib/src/screens/tab_bar.dart
@@ -6,6 +6,24 @@ class TabBarScreen extends StatefulWidget {
 }
 
 class _TabBarScreenState extends State<TabBarScreen> {
+
+
+
+  int _currentIndex;
+
+   @override
+  void initState() {
+    super.initState();
+    _currentIndex=0;
+  }
+
+
+  _onPageSelected(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,13 +32,13 @@ class _TabBarScreenState extends State<TabBarScreen> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        currentIndex: 1,
         showSelectedLabels: false,
+        onTap: _onPageSelected,
+        currentIndex: _currentIndex,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             title: Text(''),
-
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.search),
