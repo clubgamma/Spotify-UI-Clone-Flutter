a66f67f71f89abcec5c20bc60d13d6305dd55e06
Michael Dadzie <dadziemikke2@gmail.com>
2020-10-08 16:49:05 +0000

    added Settings icon on Home Screen appbar

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:spotify_ui_clone/src/screens/settings_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Home',
-          style: TextStyle(color: Colors.white),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(18, 18, 18, 1),
+        actions: [
+          IconButton(
+            splashColor: Colors.black,
+            highlightColor: Colors.black,
+            color: Colors.white,
+            icon: Icon(MaterialCommunityIcons.settings_outline),
+            onPressed: () {
+              Navigator.of(context).pushNamed(SettingsScreen.routeName);
+            },
+          )
+        ],
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+            'Home',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       ),
     );
