13b7d809e4f4ae5c7d3004802c190bc11efaa6c4
Alenjojo <alenjojo252001@icloud.com>
2020-10-10 12:29:04 +0530

    Created music file lists- vertical and horizontal in the home screen and browse all ,top genres in the search also the search bar.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:spotify_ui_clone/src/screens/tab_bar.dart';
+
 
 class SpotifyClone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Spotify-ClubGamma',
-      themeMode: ThemeMode.dark,
       theme: ThemeData(
         canvasColor: Color.fromRGBO(18, 18, 18, 1),
         primaryColor: Color.fromRGBO(30, 215, 96, 1),
         accentColor: Colors.grey,
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: Color.fromRGBO(40, 40, 40, 1),
-          elevation: 4,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Color.fromRGBO(119, 119, 119, 1),
-        ),
+        // bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        //   backgroundColor: Color.fromRGBO(40, 40, 40, 1),
+        //   elevation: 4,
+        //   showSelectedLabels: false,
+        //   showUnselectedLabels: false,
+        //   selectedItemColor: Colors.white,
+        //   unselectedItemColor: Color.fromRGBO(119, 119, 119, 1),
+        // ),
       ),
-      home: TabBarScreen(),
+ //     home: TabBarScreen(),
     );
   }
 }
