b05fc6308d1084dfe93acd4391226cd3078f3963
Alen Jojo <51394913+Alenjojo@users.noreply.github.com>
2020-10-13 12:24:46 +0530

    Aligned titles to start position (#20)
    * Created music file lists- vertical and horizontal in the home screen and browse all ,top genres in the search also the search bar.
    
    * Created music file lists- vertical and horizontal in the home screen and browse all ,top genres in the search also the search bar1.
    
    * Aligned the titles to start position

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:spotify_ui_clone/src/app.dart';
+import 'package:spotify_ui_clone/src/screens/home.dart';
 
-void main(){
-  runApp(SpotifyClone());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Spotify Clone',
+      theme: ThemeData(
+          primaryColor: Color(0xff222326),
+          accentColor: Color(0xff121212)
+      ),
+      home: HomePage(),
+    );
+  }
 }
